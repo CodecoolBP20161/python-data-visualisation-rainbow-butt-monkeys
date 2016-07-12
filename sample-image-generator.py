eb1967ae2cf38c1a07f515cedbb38bba1590ecff
makaimark <makaimark@gmail.com>
2016-07-12 14:52:42 +0200

    we have converted RGB colors, and we can read from DB

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -1,7 +1,9 @@
+from textbox import TextBox
+import psycopg2
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-
+'''
 img = Image.new("RGB", (512, 512), "red")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
@@ -17,3 +19,38 @@ draw.text((0, text_size[1]), text_content, **text_options)
 draw.text((text_size[0], 0), text_content, **text_options)
 draw.text(text_size, text_content, **text_options)
 img.save('sample-out.png')
+'''
+connect_str = "dbname = 'makaimark' user = 'makaimark' host = 'localhost' password = '920410'"
+conn = psycopg2.connect(connect_str)
+# prepare a cursor object using cursor() method
+cursor = conn.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql_1 = "SELECT company_name, COUNT(name) FROM project \
+       GROUP BY company_name"
+sql_2 = "SELECT company_name, main_color FROM project"
+
+try:
+    # Execute the SQL command
+    # cursor.execute(sql_1)
+    # Fetch all the rows in a list of lists.
+    # results_1 = cursor.fetchall()
+    cursor.execute(sql_2)
+    results_2 = cursor.fetchall()
+    dictionary = {}
+    for row in results_2:
+        if row[0] not in dictionary:
+            dictionary.setdefault(row[0], [])
+            dictionary[row[0]].append(row[1])
+        else:
+            dictionary.setdefault(row[0], []).append(row[1])
+
+    # print(dictionary)
+    for k, v in dictionary.items():
+        v = TextBox.avg_color(v)
+        print(v)
+    # print(dictionary)
+
+
+except Exception as e:
+    print(e)
---
+++ textbox.py
@@ -0,0 +1,33 @@
+
+class TextBox():
+    company_name = ""
+    color = (0, 0, 0)
+    size = 0
+
+    def __init__(self, name, color, size):
+        self.company_name = name
+        self.color = color
+        self.size = size
+
+    @staticmethod
+    def hex_to_rgb(value):
+        if value is None:
+            return (0,0,0)
+        value = value.lstrip('#')
+        lv = len(value)
+        if lv == 1:
+            v = int(value, 16)*17
+            return v, v, v
+        if lv == 3:
+            return tuple(int(value[i:i+1], 16)*17 for i in range(0, 3))
+        return tuple(int(value[i:i+lv/3], 16) for i in range(0, lv, lv/3))
+
+    @staticmethod
+    def avg_color(list):
+        rgb_sum = (0, 0, 0)
+        size = (len(list))
+        for i in range(size):
+            rgb = TextBox.hex_to_rgb(list[i])
+            rgb_sum = (rgb_sum[0] + rgb[0], rgb_sum[1]+rgb[1], rgb_sum[2]+rgb[2])
+        avg_rgb = (int(rgb_sum[0]/size), int(rgb_sum[1]/size), int(rgb_sum[2]/size))
+        return avg_rgb
