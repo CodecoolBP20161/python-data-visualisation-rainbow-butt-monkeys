133ff440d0d3a9beafda32d90060d7ffba143872
Kincses Dani <danikincs@gmail.com>
2016-07-13 15:18:07 +0200

    template

--- config-template.py
+++ config-template.py
@@ -5,3 +5,4 @@ password = password
 
 # change the file name to config.py, and the variables to your "username" etc. Import this to your
 # main python file, and refer like : connect_str = "dbname = %s user = %s host = %s password = %s" % (config.dbname, config.name, config.host, config.password)
+# put config.py in gitignore
